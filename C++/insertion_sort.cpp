bc0658cf257b5200a93264003c0ba53cbbc95701
Vasav <vasav2002@gmail.com>
2021-10-27 12:38:22 +0530

    Create insertion_sort.cpp

---
+++ C++/insertion_sort.cpp
@@ -0,0 +1,36 @@
+/* INSERTION SORT */
+/* Insert an element from unsorted array to its correct position in sorted array */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array : " << endl;
+    for (int k = 0; k < n; k++)
+    {
+        cin >> arr[k];
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i -1;
+        while(arr[j]>current && j>=0)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+    cout << "Elements of array after sorting are : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
