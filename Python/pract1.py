2d460cff740008eb4c751e2bddd643ce78f031fa
Akash2001 <akashngadhave@gmail.com>
2021-10-27 20:05:51 +0530

    Added pract1.py to read from

---
+++ Python/pract1.py
@@ -0,0 +1,32 @@
+#Practical no 1
+#Program to determine basic data types in python
+print("Demonstrating basic data types in python\n")
+
+#Numbers
+a=10
+b=20.0
+c=10+7j
+print("Examples of number data types")
+print(a)
+print(b)
+print(c)
+
+#String
+a="My name is Akash"
+print("\nExamples of string data types")
+print(a)
+
+#List
+a=[1,2,3,4,5]
+print("\nExamples of list data types")
+print(a)
+
+#Tuple
+a=(1,2,3,4,5)
+print("\nExamples of tuple data types")
+print(a)
+
+#Dictionary
+a={1:"One",2:"Two"}
+print("\nExamples of dictionary data types")
+print(a)
