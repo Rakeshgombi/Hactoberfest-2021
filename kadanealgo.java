8fb12057e6975c00de71cdf3514dae708852c08e
NEERAJ RAJ <34566622+devilraj98@users.noreply.github.com>
2021-10-31 12:23:51 +0530

    kadanealgo.java

---
+++ kadanealgo.java
@@ -0,0 +1,11 @@
+Initialize:
+    max_so_far = INT_MIN
+    max_ending_here = 0
+
+Loop for each element of the array
+  (a) max_ending_here = max_ending_here + a[i]
+  (b) if(max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+  (c) if(max_ending_here < 0)
+            max_ending_here = 0
+return max_so_far
