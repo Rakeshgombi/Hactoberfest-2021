5024e686d01351db75171ccc35a3bf25f75e973e
Syeda Nowshin Ibnat <47534922+Nowshin1077@users.noreply.github.com>
2021-10-31 13:31:09 +0600

    Obj and class in C++

---
+++ C++/Class_and_Obj.cpp
@@ -0,0 +1,39 @@
+// This Program illustrate the working of
+// objects and class in C++ Programming
+
+#include <iostream>
+using namespace std;
+
+// create a class
+class Room {
+
+   public:
+    double length;
+    double breadth;
+    double height;
+
+    double calculateArea() {
+        return length * breadth;
+    }
+
+    double calculateVolume() {
+        return length * breadth * height;
+    }
+};
+
+int main() {
+
+    // create object of Room class
+    Room room1;
+
+    // assign values to data members
+    room1.length = 42.5;
+    room1.breadth = 30.8;
+    room1.height = 19.2;
+
+    // calculate and display the area and volume of the room
+    cout << "Area of Room =  " << room1.calculateArea() << endl;
+    cout << "Volume of Room =  " << room1.calculateVolume() << endl;
+
+    return 0;
+}
